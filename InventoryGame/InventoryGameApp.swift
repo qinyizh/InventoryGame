76c1ea0b4383e341ace23ed7259fb105ac8de25a
qinyi zhang <qinyizh@gmail.com>
2025-12-15 10:09:46 -0500

    Initial Commit

---
+++ InventoryGame/InventoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryGameApp.swift
+//  InventoryGame
+//
+//  Created by Qinyi zhang on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct InventoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
